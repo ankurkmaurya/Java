b65626f0e1c8849c2c13de10c457d3d85624fbd6
Ankur Maurya <ankurkmaurya@gmail.com>
2022-12-03 20:30:31 +0530

    Package for Notes added.

---
+++ Java8/Streams/src/main/java/com/streams/notes/Notes.java
@@ -0,0 +1,24 @@
+
+package com.streams.notes;
+
+
+public interface Notes {
+	
+/**
+ * This just contains notes related to Java Streams Utility
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+}
