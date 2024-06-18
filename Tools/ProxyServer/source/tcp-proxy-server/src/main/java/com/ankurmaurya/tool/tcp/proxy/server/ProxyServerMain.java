a597557a68de9b55f307fca1ec68d2f79756bb14
Ankur Maurya <ankurkmaurya@gmail.com>
2024-06-18 10:57:01 +0530

    Command Line configuration added
    Option added to configure proxy server from command line.

--- Tools/ProxyServer/source/tcp-proxy-server/src/main/java/com/ankurmaurya/tool/tcp/proxy/server/ProxyServerMain.java
+++ Tools/ProxyServer/source/tcp-proxy-server/src/main/java/com/ankurmaurya/tool/tcp/proxy/server/ProxyServerMain.java
@@ -12,11 +12,47 @@ public class ProxyServerMain {
 	private static Map<Integer, RemoteServer>  proxyRouteMap = new HashMap<>();	
 	
 	public static void main(String[] args) {
+		try {
+			if (args.length > 0) {
+				initializeCommandLineConfiguration(args);
+			} else {
+				initializeManualConfiguration();
+			}
+		} catch (Exception e) {
+			System.out.println("Exception main() : " + e.toString());
+		}
+	}
+	
+	public static void initializeCommandLineConfiguration(String[] args) {
+		try {
+			System.out.println("Proxy Server Command Line Configuration Started.");
+			for (String arg : args) {
+				if (arg.contains(",")) {
+					String[] argSplt = arg.split(",");
+					int localPort = Integer.parseInt(argSplt[0]);
+					String remoteServerIP = argSplt[1];
+					int remoteServerPort = Integer.parseInt(argSplt[2]);
+					RemoteServer remoteServer = new RemoteServer(remoteServerIP, remoteServerPort);
+					proxyRouteMap.put(localPort, remoteServer);
+				}
+			}
+			System.out.println("Proxy Server Command Line Configuration Completed.");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Starting all the configured Proxy Servers.");
+			startProxyServers();
+		} catch (Exception e) {
+			System.out.println("Exception initializeCommandLineConfiguration() : " + e.toString());
+		}
+	}
+	
+	
+	public static void initializeManualConfiguration() {
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(System.in);
 			
-			System.out.println("Proxy Server Configuration Started.");
+			System.out.println("Proxy Server Manual Configuration Started.");
 			System.out.println("Please provide Count of Proxy Servers to be created. (Value : Min. - 1, Max. - 10)");
 			int proxyCnt = Integer.parseInt(scanner.nextLine());
 
@@ -40,7 +76,7 @@ public class ProxyServerMain {
 			startProxyServers();
 			
 		} catch (Exception e) {
-			System.out.println("Exception main() : " + e.toString());
+			System.out.println("Exception initializeManualConfiguration() : " + e.toString());
 		} finally {
 			if (scanner != null) {
 				scanner.close();
